99691cd141383fabd5784be6497143c782725dcd
KoStard <rub099999720@gmail.com>
2025-02-16 22:20:28 +0100

    feat: Adding more image formats support

--- src/optics_raytracer/image_save.py
+++ src/optics_raytracer/image_save.py
@@ -1,25 +1,31 @@
 from .vec3 import Color3
-
+from PIL import Image
 
 class ImageSaver:
-    # PPM
     def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
-        self.buffer = [
-            f"""P3
-{width} {height}
-255"""]
+        # Create empty image in RGB mode
+        self.image = Image.new('RGB', (width, height))
+        self.pixels = self.image.load()
+        self.current_x = 0
+        self.current_y = 0
     
     def add_pixel(self, color: Color3):
-        self.buffer.append(self._serialize_color(color))
-    
-    def _serialize_color(self, color: Color3):
-        red = int(255.999 * color.x)
-        green = int(255.999 * color.y)
-        blue = int(255.999 * color.z)
-        return f"{red} {green} {blue}"
+        # Convert color to 8-bit RGB values
+        r = int(255.999 * color.x)
+        g = int(255.999 * color.y)
+        b = int(255.999 * color.z)
+        
+        # Set pixel and move to next position
+        self.pixels[self.current_x, self.current_y] = (r, g, b)
+        
+        # Update position
+        self.current_x += 1
+        if self.current_x >= self.width:
+            self.current_x = 0
+            self.current_y += 1
 
     def save(self, filename: str):
-        with open(filename, 'w') as f:
-            f.write('\n'.join(self.buffer))
+        # Save image with format determined by extension
+        self.image.save(filename)
