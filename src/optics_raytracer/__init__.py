4fb3086aebb97ffc0dff7a583eb3acca0b06803d
KoStard <rub099999720@gmail.com>
2025-02-16 22:25:33 +0100

    feat: Set up __init__

--- src/optics_raytracer/__init__.py
+++ src/optics_raytracer/__init__.py
@@ -1,2 +1,36 @@
-def hello() -> str:
-    return "Hello from optics-raytracer!"
+from .camera import Camera
+from .circle import Circle
+from .cli import parse_config
+from .colored_object import ColoredObject
+from .engine import OpticsRayTracingEngine
+from .export_3d import Exporter3D
+from .image_save import ImageSaver
+from .inserted_image import InsertedImage
+from .lens import Lens
+from .pixelated_viewport import PixelatedViewport
+from .ray import Ray
+from .rectangle import Rectangle
+from .size import IntegerSize, FloatSize
+from .surface import Surface
+from .vec3 import Vec3, Point3, Color3
+
+__all__ = [
+    'Camera',
+    'Circle',
+    'ColoredObject',
+    'Color3',
+    'Exporter3D',
+    'FloatSize',
+    'ImageSaver',
+    'IntegerSize',
+    'InsertedImage',
+    'Lens',
+    'OpticsRayTracingEngine',
+    'parse_config',
+    'PixelatedViewport',
+    'Point3',
+    'Ray',
+    'Rectangle',
+    'Surface',
+    'Vec3'
+]
