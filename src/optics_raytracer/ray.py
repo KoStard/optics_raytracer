6579bf1e39c1fdfb62d4167f8296f3ba0c0eb9b0
KoStard <ruben.kostard@gmail.com>
2025-02-22 20:36:38 +0100

    iteration

--- src/optics_raytracer/ray.py
+++ src/optics_raytracer/ray.py
@@ -1,23 +1,7 @@
-from .vec3 import Vec3, Point3
+import numpy as np
+from .primitives import vector_dtype
 
-class Ray:
-    def __init__(self, origin: Point3 = None, direction: Vec3 = None):
-        self._origin = origin
-        self._direction = direction
-    
-    @property
-    def origin(self):
-        return self._origin
-
-    @property
-    def direction(self):
-        return self._direction
-    
-    def at(self, t: float):
-        return self._origin + t * self._direction
-
-    def __str__(self):
-        return f"{self._origin} + t * {self._direction}"
-    
-    def __repr__(self):
-        return f"ray(origin={repr(self._origin)}, direction={repr(self._direction)})"
+ray_dtype = np.dtype([
+    ('origin', *vector_dtype),
+    ('direction', *vector_dtype),
+])
